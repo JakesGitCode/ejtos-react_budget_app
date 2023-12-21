efe73b262fd21ba2860b0ecbff40c91ee6db986f
JakesGitCode <jakeclarkemail@gmail.com>
2023-12-20 23:42:43 -0500

    budg all

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,18 @@
+// src/components/Remaining.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Remaining = () => {
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    );
+};
+
+export default Remaining;
 
